c4ee41c323a39a0da1b99d9de04d4344eeb0f1c4
samer murad <samermurad555@gmail.com>
2019-05-18 22:39:28 +0200

    env var imports

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@ def _set_env_var(name, default):
     except KeyError:
         return default
 
-HOST = _set_env_var('SERVER_HOST', '0.0.0.0')
-PORT = _set_env_var('SERVER_PORT', 3000)
+HOST = _set_env_var('HOST', '0.0.0.0')
+PORT = _set_env_var('PORT', 3000)
 DB_URL = _set_env_var('DB_URL', 'postgresql://localhost:5432/leader_board')
